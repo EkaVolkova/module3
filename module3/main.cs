5890077ebef619dcc3de98e187daab4d5e2733bc
Ekaterina Volkova <koweevaea@gmail.com>
2023-07-26 20:03:43 +0500

    Добавление вывода литералл

--- module3/main.cs
+++ module3/main.cs
@@ -5,10 +5,13 @@ class MainClass
 	public static void Main(string[] args)
 	{
 
-		string myName;
-		myName = "Kate";
+		const string myName = "Kate";
 
 		Console.WriteLine(myName);
+
+		Console.WriteLine("Привет, мир");
+		Console.WriteLine("Мне 27 лет");
+		Console.WriteLine($"My name is Kate");
 
 		Console.ReadKey();
 	}
