313224c97aa7a9465322a112a4956492d39b3074
Ekaterina Volkova <koweevaea@gmail.com>
2023-07-26 21:16:15 +0500

    Задание модуля 3.7

---
+++ module3.7/Days.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace module3._7
+{
+    partial class main
+    {
+        enum Days : Byte
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ module3.7/main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace module3._7
+{
+    partial class main
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.Write("What is your favorite day? ");
+            Days day = checked((Days)int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Your favorite day is {day}");
+            Console.ReadKey();
+        }
+    }
+}
