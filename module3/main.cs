1d98db151195f309f79d79a00ec3cc16f948c6dc
Ekaterina Volkova <koweevaea@gmail.com>
2023-07-26 20:13:59 +0500

    Добавила вывод имени по переменной

--- module3/main.cs
+++ module3/main.cs
@@ -9,7 +9,7 @@ class MainClass
 
 		Console.WriteLine(myName);
 
-        Console.WriteLine("\t\tПривет, мир\n\t\tМне 26 лет\n\t\tMy name is\nKate");
+        Console.WriteLine($"\t\tПривет, мир\n\t\tМне 26 лет\n\t\tMy name is\n{myName}");
 		Console.ReadKey();
 	}
 }
