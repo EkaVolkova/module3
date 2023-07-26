3f24a7889d021da42f1893910710cc6c4f8b72f2
Ekaterina Volkova <koweevaea@gmail.com>
2023-07-26 20:38:54 +0500

    Задание модуля 3.3

---
+++ module3.3/Semaphore.cs
@@ -0,0 +1,9 @@
+﻿namespace module3._3
+{
+    enum Semaphore : ushort
+    {
+        Red = 100,
+        Yellow = 200,
+        Green = 300
+    }
+}
---
+++ module3.3/main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace module3._3
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
