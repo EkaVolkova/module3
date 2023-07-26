b42e28f702d1e074394db67e57e8fc5894bf0453
Ekaterina Volkova <koweevaea@gmail.com>
2023-07-26 21:05:12 +0500

    Задание модуля 3.6

---
+++ module3.6/main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace module3._6
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            double result = 10 % 3;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
